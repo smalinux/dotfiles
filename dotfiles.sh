bb8fbd316a137d16138da91071d2f88549bab831
Sohaib Mohammed <smalinux@pc.pc-domain>
2020-10-03 08:44:49 +0200

    adding suckless directory!

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,15 +1,17 @@
 # Master tools: remember!!
-sudo dnf install git # git clone https://github.com/smalinux/dotfiles.git
+yes | sudo apt-get install git # git clone https://github.com/smalinux/dotfiles.git
 # run command bash dotfiles.sh
 
 # Basic sys libs
 ##########
+yes | sudo apt-get install build-essential gcc make xorg xserver-xorg \
+	linux-headers-4.19.0-10-all-i386 firmware-linux-free mesa-utils wget\
+       	curl i3lock imagemagick feh fonts-font-awesome libx11-dev\
+	libxft-dev vim  curl mpv youtube-dl tree\
+	zathura
 
+# wifi hotspot utils
 
-# basic sys utilities
-##########
-sudo dnf install git vim Xorg wget curl i3lock ImageMagick \
-	feh 
 
 # My Tools
 ##########
@@ -17,9 +19,9 @@ sudo dnf install git vim Xorg wget curl i3lock ImageMagick \
 # Dependencies
 ##########
 # suckless depends
-sudo dnf install libXft libXinerama libX11
+yes | sudo apt-get install libxft-dev libxinerama-dev
 # install dwm
-sudo dnf install dwm
+yes | sudo apt-get install dwm
 
 
 
@@ -27,12 +29,12 @@ sudo dnf install dwm
 
 # 1- suckless
 mkdir ~/suckless && mkdir ~/bin/
-git clone git://git.suckless.org/dwm ~/suckless/
-git clone git://git.suckless.org/st ~/suckless/
-cd suxkless/dwm && make
-cd suxkless/st && make
+git clone git://git.suckless.org/dwm ~/suckless/dwm
+git clone git://git.suckless.org/st ~/suckless/st
+cd suxkless/dwm && make clean install
+cd suxkless/st && make clean install
 cd ~
 ln -sf ~/suckless/dwm ~/bin/dwm
 ln -sf ~/suckless/st ~/bin/st
 
-cat "exec dwm" > ~/.xinitrc
+echo "exec dwm" > ~/.xinitrc
