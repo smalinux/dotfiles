c8cc630b1aad5f822b8b8b9c261ad8542904fd41
Sohaib Mohamed <sohaib.amhmd@gmail.com>
2021-09-17 14:14:03 +0200

    dotfiles.sh: Cleanup

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+# (C) 2021 Sohaib Mohamed.
+# sohaib.amhmd@gmail.com
+
+
+# OS = get distro name
+OS=`awk -F= '/^NAME/{print $2}' /etc/os-release`
+
 # CapLK is a useless key in a great location
 
 # Now: I use gnome-tweak-tool for maping keys..
@@ -12,9 +20,6 @@ mkdir ~/.config/nvim/
 touch ~/.config/nvim/init.vim
 # }}}
 
-# OS = get the distro name
-OS=`awk -F= '/^NAME/{print $2}' /etc/os-release`
-
 # {{{ Ubuntu apt dependencies
 if [ "$OS" = "\"Ubuntu\"" ];
 then
@@ -28,18 +33,18 @@ then
 fi
 # }}}
 
-# .git config
-#sudo chmod +x ~/dotfiles/git.sh
+# {{{ .git config
 sh ~/dotfiles/git.sh
+# }}}
 
 # {{{ Mute Gnome alerts
 gsettings set org.gnome.desktop.sound event-sounds false
-cd ~
-mkdir bin
 # }}}
 
 # {{{ soft links
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/i3/config ~/.config/i3/config
+ln -sf ~/dotfiles/terminator/config ~/.config/terminator/config
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/update.sh ~/update.sh
 ln -sf ~/dotfiles/.Xmodmap ~/.Xmodmap
@@ -55,8 +60,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # }}}
 
 # {{{ Installing all vim plugins and quit.
-vim +PluginInstall +qall # KEEB THIS LINE ALWAYS LAST LINE !
-cd ~/.vim/bundle/youcompleteme && ./install.py --clang-completer && python install.py
+vim +PluginInstall +qall
 # }}}
 
 # {{{ install Tabular
@@ -92,29 +96,31 @@ echo "___________________________"
 echo "Open GNOME terminal preferences > Profiles > scrolling > Scrollbar is: (Disable)"
 # }}}
 
-# Remove user passwd to speed ur work
-# https://www.google.com/search?q=fedora+change+passwd+user+to+empty
-echo "========== IF you want to speed you work >> remove usr passwd =========="
-echo "sudo passwd -r root"
-echo "sudo passwd -r -f -u smalinux"
-echo "========================================================================"
+## {{{  Remove user passwd to speed ur work
+## https://www.google.com/search?q=fedora+change+passwd+user+to+empty
+#echo "========== IF you want to speed you work >> remove usr passwd =========="
+#echo "sudo passwd -r root"
+#echo "sudo passwd -r -f -u smalinux"
+## }}}
 
-# Fuck off mouse! I will never to back again! I'm vimmer who use GUI
-#   with keyboard ONLY!..........
-echo "Please if you are moving between pysical an virtul vmware, to this:"
-echo "	VMware > edit > preferences > input"
-echo "		<check> grap keyboard and mouse on key click"
-echo "========================================================================"
-
-# VMware related stuff
-sh ~/dotfiles/VMware.sh
-
+## {{{ Fuck off mouse! I will never to back again! I'm vimmer who use GUI
+##   with keyboard ONLY!..........
+#echo "Please if you are moving between pysical an virtul vmware, to this:"
+#echo "	VMware > edit > preferences > input"
+#echo "		<check> grap keyboard and mouse on key click"
+#echo "========================================================================"
+## }}}
+#
+## {{{ VMware related stuff
+#sh ~/dotfiles/VMware.sh
+## }}}
 
 #echo "ToDO:"
 #echo "Google: vim youcompleteme linux kernel" # how to use ycm plugin with kernel
 
-# change this maximum screencast length to a different value
+# {{{ change this maximum screencast length to a different value
 gsettings set org.gnome.settings-daemon.plugins.media-keys max-screencast-length 60000
+# }}}
 
 # take keybindings of "less" and but them into "most"
 # reference: https://unix.stackexchange.com/a/271252
@@ -124,4 +130,4 @@ cp /usr/share/doc/most/lesskeys.rc ~/.mostrc
 rm ~/.local/share/keyrings/login.keyring
 
 
-echo "that's it!"
+echo "that's it! have fun! - smalinux"
