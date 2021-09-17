8732df6045235fc6c1388fbeda41a6d28bd0029b
Sohaib Mohamed <sohaib.amhmd@gmail.com>
2021-09-17 12:31:54 +0200

    Cleanup dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -7,24 +7,38 @@
 
 # Later: try to make j & k keybindings faster : https://youtu.be/d8XtNXutVto?t=492
 
-
-# Setup Neovim
+# {{{ Setup Neovim
 mkdir ~/.config/nvim/
 touch ~/.config/nvim/init.vim
+# }}}
 
-# fedora yum dependencies
-sudo chmod +x ~/dotfiles/yum.sh
-sudo sh ~/dotfiles/yum.sh
+# OS = get the distro name
+OS=`awk -F= '/^NAME/{print $2}' /etc/os-release`
+
+# {{{ Ubuntu apt dependencies
+if [ "$OS" = "\"Ubuntu\"" ];
+then
+   sudo sh ~/dotfiles/apt.sh
+fi
+
+# {{{ fedora yum dependencies
+if [ "$OS" = "\"Fedora\"" ];
+then
+   sudo sh ~/dotfiles/yum.sh
+fi
+# }}}
 
 # .git config
-sudo chmod +x ~/dotfiles/git.sh
+#sudo chmod +x ~/dotfiles/git.sh
 sh ~/dotfiles/git.sh
 
-# Mute Gnome alerts
+# {{{ Mute Gnome alerts
 gsettings set org.gnome.desktop.sound event-sounds false
 cd ~
 mkdir bin
+# }}}
 
+# {{{ soft links
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/update.sh ~/update.sh
@@ -33,29 +47,58 @@ ln -sf ~/dotfiles/init.vim ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/.tigrc ~/.tigrc
 sudo ln -sf ~/dotfiles/soh /usr/share/X11/xkb/symbols/soh
 ln -sf ~/dotfiles/keys.sh ~/keys.sh
+# }}}
 
-# install Tabular
+# {{{ install vundleVim plugin manager
+mkdir -p ~/.vim/bundle/Vundle.vim/
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# }}}
+
+# {{{ Installing all vim plugins and quit.
+vim +PluginInstall +qall # KEEB THIS LINE ALWAYS LAST LINE !
+cd ~/.vim/bundle/youcompleteme && ./install.py --clang-completer && python install.py
+# }}}
+
+# {{{ install Tabular
 cd ~
 mkdir -p ~/.vim/bundle
 cd ~/.vim/bundle
 git clone git://github.com/godlygeek/tabular.git
+# }}}
 
-# install vundleVim plugin manager
-mkdir -p ~/.vim/bundle/Vundle.vim/
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-# Installing all vim plugins and quit.
-vim +PluginInstall +qall # KEEB THIS LINE ALWAYS LAST LINE !
-cd ~/.vim/bundle/youcompleteme && ./install.py --clang-completer && python install.py
-### END OF FILE
+# {{{ cache GitHub passwd
+# Reference: https://stackoverflow.com/a/5343146/5688267
 
+# if Ubuntu
+if [ "$OS" = "\"Ubuntu\"" ];
+then
+   cd /usr/share/doc/git/contrib/credential/libsecret
+   sudo make
+   git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
+fi
+
+# if Fedora
+
+if [ "$OS" = "\"Fedora\"" ];
+then
+   sudo dnf install git-credential-libsecret
+   git config --global credential.helper /usr/libexec/git-core/git-credential-libsecret
+fi
+# }}}
+
+# {{{ Disable GNOME's Scrollbars
+echo "Disable GNOME's Scrollbars:"
+echo "___________________________"
+echo "Open GNOME terminal preferences > Profiles > scrolling > Scrollbar is: (Disable)"
+# }}}
 
 # Remove user passwd to speed ur work
 # https://www.google.com/search?q=fedora+change+passwd+user+to+empty
-echo "========================================================================"
 echo "========== IF you want to speed you work >> remove usr passwd =========="
 echo "sudo passwd -r root"
 echo "sudo passwd -r -f -u smalinux"
 echo "========================================================================"
+
 # Fuck off mouse! I will never to back again! I'm vimmer who use GUI
 #   with keyboard ONLY!..........
 echo "Please if you are moving between pysical an virtul vmware, to this:"
@@ -67,13 +110,10 @@ echo "========================================================================"
 sh ~/dotfiles/VMware.sh
 
 
-echo "ToDO:"
-echo "Google: vim youcompleteme linux kernel" # how to use ycm plugin with kernel
+#echo "ToDO:"
+#echo "Google: vim youcompleteme linux kernel" # how to use ycm plugin with kernel
 
-# tweaks
-./~/dotfiles/tweaks.sh
-
-" change this maximum screencast length to a different value
+# change this maximum screencast length to a different value
 gsettings set org.gnome.settings-daemon.plugins.media-keys max-screencast-length 60000
 
 # take keybindings of "less" and but them into "most"
--- tweaks.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Disable GNOME's Scrollbars
-echo "Disable GNOME's Scrollbars:"
-echo "Open GNOME terminal preferences > Profiles > scrolling > Scrollbar is: (Disable)"
