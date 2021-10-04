77329bde992af1f73e49f438f80117ec7dc855a1
Sohaib Mohamed <sohaib.amhmd@gmail.com>
2021-10-04 05:17:36 +0200

    FreeBSD: Disable sendmail on boot
    Signed-off-by: Sohaib Mohamed <sohaib.amhmd@gmail.com>

--- dotfiles-pkg.sh
+++ dotfiles-pkg.sh
@@ -5,3 +5,7 @@ echo "exec /usr/local/bin/i3" > ~/.xinitrc
 #shutdown -r now
 #startx
 # }}}
+
+echo 'sendmail_submit_enable="NO"' >> /etc/rc.conf
+echo 'sendmail_outbound_enable="NO"' >> /etc/rc.conf
+echo 'sendmail_msp_queue_enable="NO"' >> /etc/rc.conf
