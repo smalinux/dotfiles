cd7850d228fcd785aeb1562587f0a276ed4caf84
Sohaib Mohamed <sohaib.amhmd@gmail.com>
2021-10-04 05:27:57 +0200

    FreeBSD: hello, world!
    suckless dwm, st, pkg.sh, .... etc
    
    Signed-off-by: Sohaib Mohamed <sohaib.amhmd@gmail.com>

--- dotfiles-pkg.sh
+++ dotfiles-pkg.sh
@@ -1,7 +1,9 @@
 # {{{ Setup i3
-echo "dbus_enable=\"YES\"" >> /etc/rc.conf
-echo "hald_enable=\"YES\"" >> /etc/rc.conf
-echo "exec /usr/local/bin/i3" > ~/.xinitrc
+echo 'dbus_enable="YES"' >> /etc/rc.conf
+echo 'hald_enable="YES"' >> /etc/rc.conf
+echo 'exec /usr/local/bin/i3' > ~/.xinitrc
+#cp ~/dotfiles/i3/config /root/.config/i3/config
+wget https://raw.githubusercontent.com/deadesthead/lilrepo/master/config/i3/.i3/config ~/.config/i3/
 #shutdown -r now
 #startx
 # }}}
--- pkg.sh
+++ pkg.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 # First of all please, cp all dotfile/.something to ur ~
 yes | pkg install most tree atop gcc htop tig neovim p5-ack
-yes | pkg install xorg
-yes | pkg install i3 i3status i3lock dmenu
+yes | pkg install xorg pkgconf
+yes | pkg install awesome feh compton devel/bear
+yes | pkg install tree bash autoconf autotools devel/ncurses
+yes | pkg install node npm ccls
+yes | pkg install maim
+
+
+chsh -s /usr/local/bin/bash
