84f246ec0066b0dc4fa5fab4ffb9dc8d223f84fd
Sohaib Mohamed <sohaib.amhmd@gmail.com>
2021-10-09 06:03:36 +0200

    `smlinks.sh`: for all my soft links
    This make my life more moduler :)
    
    Signed-off-by: Sohaib Mohamed <sohaib.amhmd@gmail.com>

--- dotfiles.sh
+++ dotfiles.sh
@@ -42,17 +42,7 @@ gsettings set org.gnome.desktop.sound event-sounds false
 # }}}
 
 # {{{ soft links
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/i3/ ~/.config/
-ln -sf ~/dotfiles/terminator/ ~/.config/
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/update.sh ~/update.sh
-ln -sf ~/dotfiles/.Xmodmap ~/.Xmodmap
-ln -sf ~/dotfiles/init.vim ~/.config/nvim/init.vim
-ln -sf ~/dotfiles/.tigrc ~/.tigrc
-sudo ln -sf ~/dotfiles/soh /usr/share/X11/xkb/symbols/soh
-ln -sf ~/dotfiles/keys.sh ~/keys.sh
-ln -sf ~/dotfiles/coc-settings.json ~/.config/nvim/coc-settings.json
+bash ~/dotfiles/smlinks.sh
 # }}}
 
 # {{{ Setup .gitconfig
---
+++ smlinks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/i3/ ~/.config/
+ln -sf ~/dotfiles/terminator/ ~/.config/
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/update.sh ~/update.sh
+ln -sf ~/dotfiles/.Xmodmap ~/.Xmodmap
+ln -sf ~/dotfiles/init.vim ~/.config/nvim/init.vim
+ln -sf ~/dotfiles/.tigrc ~/.tigrc
+ln -sf ~/dotfiles/.dwm ~/
+sudo ln -sf ~/dotfiles/soh /usr/share/X11/xkb/symbols/soh
+ln -sf ~/dotfiles/keys.sh ~/keys.sh
+ln -sf ~/dotfiles/coc-settings.json ~/.config/nvim/coc-settings.json
+sudo ln -sf ~/dotfiles/xsessions/dwm.desktop /usr/share/xsessions/dwm.desktop
