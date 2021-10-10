06ad2c6a837e71a99e05daae1ebc187ac09beab0
Sohaib Mohamed <sohaib.amhmd@gmail.com>
2021-10-10 18:53:22 +0200

    .dwm/autostart.sh update
    Signed-off-by: Sohaib Mohamed <sohaib.amhmd@gmail.com>

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -3,12 +3,18 @@
 # compton
 compton &
 
-# add arabic keyboeard layout
-setxkbmap -layout us,ar
-setxkbmap -option 'grp:win_space_toggle'
-
 # caps lock <-> esc
 setxkbmap -option caps:swapescape
 
+# add arabic keyboeard layout
+setxkbmap -layout us,ar
+setxkbmap -option 'grp:ctrls_toggle'
+
+
 # wallpaper
 feh --bg-fill --randomize https://unsplash.it/1920/1080?random
+
+# make j & k keybindings faster : https://youtu.be/d8XtNXutVto?t=492
+exec_always xset r rate 300 50
+
+strc
