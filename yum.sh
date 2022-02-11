c447e2427e056451968cfc6b2ea6d2d29e9c658b
Sohaib Mohamed <sohaib.amhmd@gmail.com>
2022-02-11 22:46:54 +0200

    Fixup: yum.sh: update
    Signed-off-by: Sohaib Mohamed <sohaib.amhmd@gmail.com>

--- yum.sh
+++ yum.sh
@@ -7,8 +7,10 @@ yes | sudo yum install qemu nasm libmpc-devel cmake ack vim
 # please don't depend on this packages of ever.
 # depend on istractions on GitHub: https://github.com/performancecopilot/pcp/blob/master/INSTALL.md
 # I wrote them for quick start & backup reasons. [NOW: 15/1/2021]
-yes | sudo yum install mesa-libGL-devel flex-devel bison-devel qt-devel ragel pylint mandoc cppcheck libuv-devel libvarlink-devel rdma-core-devel infiniband-diags-devel avahi-devel libpfm-devel rpm-devel readline-devel python3-jsonpointer python3-rtslib libvirt-python3 python3-prometheus_client python3-elasticsearch python3-openpyxl python3-bcc bpftrace python3-psycopg2 python3-pyodbc perl-File-Slurp rrdtool-perl socat nmap valgrind  mailx 389-ds-base cyrus-sasl-md5 sysstat avahi-tools kubernetes-client perl-DBD-mysql memcached zabbix-agent mysql postgresql gfs2-utils docker qtchooser qt5-qtbase-devel qtchooser qt5-qtsvg-devel SoQt-devel Coin4-devel SoQt-devel setools-console selinux-policy-devel smartmontools lm_sensors redis RediSearch device-mapper-devel chan-devel HdrHistogram_c-devel postfix-perl-scripts python2-setuptools libibmad-devel infiniband-diags-devel libibmad-devel perl-YAML-LibYAML perl-XML-TokeParser cpan perl-Spreadsheet-XLSX perl-Net-SNMP RediSearch
-infiniband-diags-devel
+yes | sudo yum install mesa-libGL-devel flex-devel bison-devel qt-devel ragel pylint mandoc cppcheck libuv-devel libvarlink-devel rdma-core-devel infiniband-diags-devel avahi-devel libpfm-devel rpm-devel readline-devel python3-jsonpointer python3-rtslib libvirt-python3 python3-prometheus_client python3-elasticsearch python3-openpyxl python3-bcc bpftrace python3-psycopg2 python3-pyodbc perl-File-Slurp rrdtool-perl socat nmap valgrind  mailx 389-ds-base cyrus-sasl-md5 sysstat avahi-tools kubernetes-client perl-DBD-mysql memcached zabbix-agent mysql postgresql gfs2-utils docker qtchooser qt5-qtbase-devel qtchooser qt5-qtsvg-devel SoQt-devel Coin4-devel SoQt-devel setools-console selinux-policy-devel smartmontools lm_sensors redis RediSearch device-mapper-devel chan-devel HdrHistogram_c-devel postfix-perl-scripts libibmad-devel infiniband-diags-devel libibmad-devel perl-YAML-LibYAML perl-XML-TokeParser cpan perl-Spreadsheet-XLSX perl-Net-SNMP RediSearch infiniband-diags-devel cyrus-sasl-devel kubernetes-node cronie python3-devel nspr-devel nss-devel RediSearch libibmad-devel infiniband-diags-devel infiniband-diags-devel perl-JSON
+
+yes | sudo cpan install XML::LibXML Spreadsheet::Read Net::LDAP
+
 # END PCP preformance Copilot
 
 yes | su -c "yum install gparted"
