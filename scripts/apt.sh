c9f63ed6eb958bc447d55667968c044934500d06
Sohaib Mohamed <sohaib.amhmd@gmail.com>
2023-03-05 03:52:05 +0200

    apt.sh: update
    Signed-off-by: Sohaib Mohamed <sohaib.amhmd@gmail.com>

--- scripts/apt.sh
+++ scripts/apt.sh
@@ -61,7 +61,6 @@ yes | sudo snap install vlc
 yes | sudo snap install ripgrep --classic
 yes | sudo snap install telegram-desktop
 yes | sudo snap install slack --classic
-yes | sudo snap install orange-app
 
 # {{{ obs-studio
 yes | sudo add-apt-repository ppa:obsproject/obs-studio
