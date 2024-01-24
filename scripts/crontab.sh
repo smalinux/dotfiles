510f18edf629a8c34019e8170fe62e7e7a8f52b7
Sohaib Mohamed <sohaib.amhmd@gmail.com>
2024-01-24 14:18:05 +0200

    `crontab.sh`: update
    Signed-off-by: Sohaib Mohamed <sohaib.amhmd@gmail.com>

--- scripts/crontab.sh
+++ scripts/crontab.sh
@@ -3,19 +3,16 @@
 pushd ~
 
 
-   # sorted!
-   cd /home/smalinux/docs/ && make push
    cd /home/smalinux/repos/pwn.college/ && make push
    cd /home/smalinux/repos/problem_solving/ && make push
    cd /home/smalinux/repos/urls/ && make push
    cd /home/smalinux/python/ && make push
    cd /home/smalinux/repos/scripts/ && make push # rand python scripts
-   # data science section
    cd /home/smalinux/web_scraping/ && make push
    cd /home/smalinux/data_science/ && make push
    cd /home/smalinux/repos/sc-backup/ && make push
-   # Update my docs repos (Obsidian) every 3 hours
-   cd /home/smalinux/docs/ && make push
+   cd /home/smalinux/docs/ && make push # Update my docs repos (Obsidian) every 3 hours
+   cd /home/smalinux/repos/c_/ && make push #FIXME: Cleanup: Add this repo to Obsidian
 
 
 popd
