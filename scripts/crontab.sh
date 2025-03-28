82c19befc7d2a9c5537175a91ac884623ef4cf0d
Sohaib Mohamed <sohaib.amhmd@gmail.com>
2025-03-28 15:29:11 +0100

    crontab: update
    Signed-off-by: Sohaib Mohamed <sohaib.amhmd@gmail.com>

--- scripts/crontab.sh
+++ scripts/crontab.sh
@@ -3,17 +3,18 @@
 pushd ~
 
 
-   cd /home/smalinux/repos/pwn.college/ && make push
-   cd /home/smalinux/repos/problem_solving/ && make push
-   cd /home/smalinux/repos/urls/ && make push
-   cd /home/smalinux/python/ && make push
-   cd /home/smalinux/repos/scripts/ && make push # rand python scripts
-   cd /home/smalinux/web_scraping/ && make push
-   cd /home/smalinux/data_science/ && make push
-   cd /home/smalinux/repos/sc-backup/ && make push
-   cd /home/smalinux/docs/ && make push # Update my docs repos (Obsidian) every 3 hours
-   cd /home/smalinux/repos/c_/ && make push #FIXME: Cleanup: Add this repo to Obsidian
-   cd /home/smalinux/repos/rss_db_backup/ && make push
+   #cd /home/smalinux/repos/pwn.college/ && make push
+   #cd /home/smalinux/repos/problem_solving/ && make push
+   #cd /home/smalinux/repos/urls/ && make push
+   #cd /home/smalinux/python/ && make push
+   #cd /home/smalinux/repos/scripts/ && make push # rand python scripts
+   #cd /home/smalinux/web_scraping/ && make push
+   #cd /home/smalinux/data_science/ && make push
+   #cd /home/smalinux/repos/sc-backup/ && make push
+   #cd /home/smalinux/docs/ && make push # Update my docs repos (Obsidian) every 3 hours
+   #cd /home/smalinux/repos/c_/ && make push #FIXME: Cleanup: Add this repo to Obsidian
+   #cd /home/smalinux/repos/rss_db_backup/ && make push
+   cd /home/smalinux/repos/smalinux.github.io/ && make push
 
 
 popd
